d60d306d1112851347f293a7327912730486cb5e
ItamarShmelo <itamargiron@gmail.com>
2024-02-05 17:21:19 +0200

    constructor with random seed for sample_uniform_01

--- src/tau_matrix_monte_carlo.cpp
+++ src/tau_matrix_monte_carlo.cpp
@@ -6,3 +6,20 @@
 #include "units.hpp"
 
 static double constexpr signaling_NaN = std::numeric_limits<double>::signaling_NaN();
+
+tau_matrix_monte_carlo_engine::tau_matrix_monte_carlo_engine(Vector const energy_groups_center_, 
+                                                             Vector const energy_groups_boundries_, 
+                                                             std::size_t const num_of_samples_) :
+                            energy_groups_center(energy_groups_center_),
+                            energy_groups_boundries(energy_groups_boundries_),
+                            num_energy_groups(energy_groups_center.size()),
+                            tau_temp(num_energy_groups, Vector(num_energy_groups, signaling_NaN)),
+                            num_of_samples(num_of_samples_), 
+                            T(signaling_NaN),
+                            theta(signaling_NaN),
+                            sum_1_bt(signaling_NaN),
+                            Sb(signaling_NaN),
+                            sample_uniform_01(
+                                boost::random::mt19937(static_cast<unsigned int>(std::time(0))),
+                                boost::random::uniform_01<>()
+                            ) {}
